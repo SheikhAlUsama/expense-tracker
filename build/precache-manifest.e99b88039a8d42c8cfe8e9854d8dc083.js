2c970b42821212258d1002a96ec492ea83d0918b
SheikhAlUsama <ussheikh001@gmail.com>
2021-10-12 08:50:37 -0700

    Expense tracker app complete

---
+++ build/precache-manifest.e99b88039a8d42c8cfe8e9854d8dc083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ea26ca0317fff14e9e56a8a2920b78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b978334ca8ae36c4226",
+    "url": "/static/css/main.b81632b9.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b978334ca8ae36c4226",
+    "url": "/static/js/main.5272de63.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
